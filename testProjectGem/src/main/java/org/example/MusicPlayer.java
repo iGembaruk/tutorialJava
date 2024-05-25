e9ea44859e99b9dee59d7e93afa0ff469cb2887e
Igor <i.gembaruk@mail.ru>
2024-05-25 18:04:54 +0300

    lesson6HomeTaskList

--- testProjectGem/src/main/java/org/example/MusicPlayer.java
+++ testProjectGem/src/main/java/org/example/MusicPlayer.java
@@ -1,18 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    //private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    //public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
     public MusicPlayer(){}
 
-    public void setMusic(Music music){
-        this.music = music;
+    public void setMusicList(List<Music> musicList){
+        this.musicList = musicList;
     }
+
+    public List<Music> getMusicList(){
+        return musicList;
+    }
+
+//    //public void setMusic(Music music){
+//        this.music = music;
+//    }
 
     public String getName() {
         return name;
@@ -31,7 +43,10 @@ public class MusicPlayer {
     }
 
     public void playMusic() {
-        System.out.println("Playing " + music.getSong());
+        for(Music element:musicList){
+            System.out.println("Playing " + element.getSong());
+        }
     }
+
 }
 
